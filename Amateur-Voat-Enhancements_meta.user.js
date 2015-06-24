5c2d49e1750ef168a9d3da16d8e0f207f598b52b
HorzaGobuchul <github@retrapur.fr>
2015-06-24 19:05:56 +0200

    Update and rename Amateur-Voat-Enhancements.user.js to Amateur-Voat-Enhancements_meta.user.js

--- Amateur-Voat-Enhancements_meta.user.js
+++ Amateur-Voat-Enhancements_meta.user.js
@@ -11,7 +11,7 @@
 // @grant       GM_setValue
 // @grant       GM_deleteValue
 // @run-at      document-end
-// @updateURL   https://github.com/HorzaGobuchul/Amateur-Voat-Enhancements/raw/master/Voat_subverses_header_meta.user.js
-// @downloadURL https://github.com/HorzaGobuchul/Amateur-Voat-Enhancements/raw/master/Voat_subverses_header.user.js
+// @updateURL   https://github.com/HorzaGobuchul/Amateur-Voat-Enhancements/raw/master/Amateur-Voat-Enhancements_meta.user.js
+// @downloadURL https://github.com/HorzaGobuchul/Amateur-Voat-Enhancements/raw/master/Amateur-Voat-Enhancements.user.js
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // ==/UserScript==
