1c66f7a0743fb89b5c276fbddfdab25e560b00dd
HorzaGobuchul <github@retrapur.fr>
2015-06-24 13:04:00 +0200

    meta file for update

---
+++ Voat_subverses_header_meta.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        Voat Enhancement Suite Lite
+// @author      Horza
+// @date        24 june 2015
+// @description Add new features to voat.co
+// @license      MIT; https://github.com/HorzaGobuchul/Amateur-Voat-Enhancements/blob/master/LICENSE
+// @match       *://voat.co/*
+// @match       *://*.voat.co/*
+// @version     0.8
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_deleteValue
+// @run-at      document-end
+// @updateURL   https://github.com/HorzaGobuchul/Amateur-Voat-Enhancements/raw/master/Voat_subverses_header_meta.user.js
+// @downloadURL https://github.com/HorzaGobuchul/Amateur-Voat-Enhancements/raw/master/Voat_subverses_header.user.js
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// ==/UserScript==
