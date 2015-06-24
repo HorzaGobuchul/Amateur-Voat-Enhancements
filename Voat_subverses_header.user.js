0b63151623b0ff5c6a557ada82999b34314e0b68
HorzaGobuchul <github@retrapur.fr>
2015-06-24 16:43:38 +0200

    v0.9

--- Voat_subverses_header.user.js
+++ Voat_subverses_header.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name        Voat Enhancement Suite Lite
+// @name        Amateur Voat Enhancements
 // @author      Horza
 // @date        24 june 2015
 // @description Add new features to voat.co
-// @license      MIT; https://github.com/HorzaGobuchul/Amateur-Voat-Enhancements/blob/master/LICENSE
+// @license     MIT; https://github.com/HorzaGobuchul/Amateur-Voat-Enhancements/blob/master/LICENSE
 // @match       *://voat.co/*
 // @match       *://*.voat.co/*
-// @version     0.8
+// @version     0.9
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_deleteValue
@@ -19,11 +19,12 @@
 'use strict';
 var data = {};
 
-data.option = {
+data.option = { //Pulled from GM_Value
     FixedAccountHeader: true,
     FixedListHeader: true,
-    ListHeaderHeight: 24,
     EnableTags: true,
+    EnableImage: true,
+    EnableSubHeader : true
 };
 
 $(window).ready(function () {
@@ -33,13 +34,18 @@ $(window).ready(function () {
     data.subverseName = GetSubverseName();
     data.isPageSubverse = isPageSubverse();
     data.currentPageType = GetCurrentPageType();
+    data.ListHeaderHeight = 24,
     data.CSSstyle = GetCSSStyle();
     
+    for (var key in data.option) {
+        data.option[key] = GM_getValue(key, true);
+    }
 
-    DisplayCustomSubversesList();
+    if (data.option.EnableSubHeader) {
+        DisplayCustomSubversesList();
+    }
 
-    if ($.inArray(data.currentPageType, ["subverses", "sets", "user", "user-manage", "mysets"]) == -1)
-    {
+    if (data.option.EnableImage && $.inArray(data.currentPageType, ["subverses", "sets", "user", "user-manage", "mysets"]) == -1) {
         AppendImageButton();
     }
     
@@ -52,11 +58,14 @@ $(window).ready(function () {
     } else if (data.currentPageType == "set") {
         AddShortcutsButtonInSetPage(); //https://voat.co/set/xx
     }
+
+    if (data.currentPageType == "user-manage") {
+        InsertAVEManager();
+    }
     
     if (data.option.EnableTags)
     {
         data.usertags = GM_getValue("Voat_Tags", "");
-        //GM_deleteValue("Voat_Tags");
         ShowUserTag();
     }
 });
@@ -208,8 +217,7 @@ function GetSubverseName() {
 
 /// Special methods related to shortcuts ///
 function GetSubversesList() {
-    var subversesArr = GM_getValue("Voat_Subverses", "newsubverses,introductions,news");
-    return subversesArr.split(',');
+    return GM_getValue("Voat_Subverses", "newsubverses,introductions,news").split(',');
 }
 
 function GetSetParam(str) {
@@ -293,7 +301,6 @@ function DisplayCustomSubversesList() {
         if (subArr[idx] == "") { continue;}
         if (data.regExpSet.test(subArr[idx])) { //ex: name:12
             setInfo = GetSetParam(subArr[idx]);
-            //use a special style for set shortcuts: bold or italic.
             SubString += '<li><span class="separator">-</span><a href="/set/' + setInfo[1] + '/" style="font-weight:bold;font-style: italic;">' + setInfo[0] + '</a></li>';
         }
         else {
@@ -383,7 +390,7 @@ function ToggleSelectedState(obj) {
     data.SelectedPost = obj;
 }
 
-//A and Z to vaot
+//A and Z to vote
 $(document).keypress(function (event) {
     if ($(":input").is(":focus")) { return; }
 
@@ -403,9 +410,9 @@ var headerAccountPos = $('#header-account').offset().top;
 $(window).scroll(function () {
     if (!data.option.FixedAccountHeader) {return;}
 
-    if ($(window).scrollTop() + (data.option.FixedListHeader ? data.option.ListHeaderHeight : 0) > headerAccountPos) {
+    if ($(window).scrollTop() + (data.option.FixedListHeader ? data.ListHeaderHeight : 0) > headerAccountPos) {
         $('#header-account').css('position', 'fixed')
-                            .css('top', data.option.FixedListHeader ? data.option.ListHeaderHeight : "0")
+                            .css('top', data.option.FixedListHeader ? data.ListHeaderHeight : "0")
                             .css('right', '0')
                             .css("text-align", "center")
                             .css("height", "0px");
@@ -429,7 +436,7 @@ $(window).scroll(function () {
         $('.width-clip').css('position', 'fixed')
                             .css('z-index', '1000')
                             .css('border-bottom', '1px solid ' + (data.CSSstyle == "dark" ? "#222" : "#DCDCDC"))
-                            .css("height", data.option.ListHeaderHeight + "px")
+                            .css("height", data.ListHeaderHeight + "px")
                             .css("background-color", data.CSSstyle == "dark" ? "#333" : "#FFF");
     } else {
         $('.width-clip').css('position', '')
@@ -482,12 +489,12 @@ function ShowUserTag() {
     var Tag_html, name, tag;
     var style = "border:1px solid #" + (data.CSSstyle == "dark" ? "5452A8" : "D1D0FE") + ";background-color:#" + (data.CSSstyle == "dark" ? "304757" : "F4FCFF") + ";font-size:10px;padding:0px 4px;color:#6CA9E4;font-weight:bold;margin-left:4px;cursor: pointer;";
 
-    //All mention on an username as a link.
+    //All mention of an username as a link.
     var sel = /\/user\/[^/]*\/?$/i;
     $("a[href*='/user/']").each(function () {
         if (!$(this).attr('href').match(sel)) return true;
         name = $(this).html().replace("@", "").replace("/u/", "").toLowerCase(); //Accepts: Username, @Username, /u/Username
-        if ($(this).attr('href').split("/")[2].toLowerCase() != name) return true;
+        if ($(this).attr('href').split("/")[2].toLowerCase() != name.toLowerCase()) return true;
 
         tag = GetTag(name);
         Tag_html = '<span class="GM_UserTag" id="' + name + '" style="' + style + '">' + (!tag ? "+" : tag) + '</span>';
@@ -501,15 +508,10 @@ function ShowUserTag() {
         Tag_html = '<span class="GM_UserTag" id="' + name + '" style="' + style + '">' + (!tag ? "+" : tag) + '</span>';
         $(".alert-title").html("Profile overview for "+name+Tag_html+".");
     }
-    
-
-    //Username in messages
-    //if ($.inArray(data.currentPageType, ["user", "user-comments", "users-submissions"]) >= 0) {}
-
 }
 
 function UpdateUserTag(name, tag) {
-    $("a[class*='GM_UserTag'][id*='" + name + "']").each(function () {
+    $("span[class*='GM_UserTag'][id*='" + name + "']").each(function () {
         $(this).text(tag);
     });
 }
@@ -517,7 +519,7 @@ function UpdateUserTag(name, tag) {
 $(document).on("click", ".GM_UserTag", function () {
     var username = $(this).attr("id");
     var oldTag = $(this).text();
-    var newTag = prompt(username, oldTag !== "+" ? oldTag : "").replace(/[:,]/g,"-");
+    var newTag = prompt("Tag for "+username, oldTag !== "+" ? oldTag : "").replace(/[:,]/g,"-");
     if (newTag.length > 0) {
         if (oldTag !== "+") {
             UpdateTag(username, newTag);
@@ -590,3 +592,49 @@ function GetTagCount() {
     return data.usertags.split(",").length;
 }
 /// END ////
+
+
+/// AVE Manager ///
+
+function InsertAVEManager() {
+    var Labels = {
+        FixedAccountHeader: "Enable fixed position for the account block",
+        FixedListHeader: "Enable fixed position for the Subverse list header",
+        EnableTags: "Enable user tags",
+        EnableImage: "Enable button to display images",
+        EnableSubHeader: "Replace Subverse list header with custom choice of shortcuts",
+    }
+
+    var MngHTML = '<br /><div class="alert-title">AVE Preferences</div>';
+    MngHTML += '<section id="AVEPreferences">'
+    MngHTML += '<form class="form-horizontal" action="/account/manage" method="get">';
+    for (var i in data.option) {
+        MngHTML += '<div class="checkbox">'+
+                       '<input '+ (data.option[i] ? 'checked="checked"' : '') +' id="' + i + '" name="' + i + '" value="' + data.option[i] + '" type="checkbox"></input>' +
+                       '<label for="' + i + '">' + Labels[i] + '</label>' +
+                   '</div>';
+    }
+
+    MngHTML += '<br /><input value="Reset Stored Data" id="AVEPrefRest" class="btn btn-whoaverse" type="submit" title="Warning: this will delete your preferences, shortcut list and all usertags!"></input>';
+    MngHTML += '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input value="Save" id="AVEPrefSave" class="btn btn-whoaverse" type="submit" title="Save!"></input>';
+    MngHTML += '</form></section><br />';
+
+    $(MngHTML).insertBefore($(".alert-title").get(2));
+}
+
+$(document).on("click", "#AVEPrefSave", function () {
+    $("#AVEPreferences").find(":checkbox").each(function () {
+        data.option[$(this).attr("id")] = $(this).is(":checked");
+        GM_setValue($(this).attr("id"), $(this).is(":checked"));
+    });
+});
+
+$(document).on("click", "#AVEPrefRest", function () {
+    for (var key in data.option)
+    {GM_deleteValue(key);}
+    GM_deleteValue("Voat_Tags");
+    GM_deleteValue("Voat_Subverses");
+});
+
+
+/// END ///
