f8e0f7474f121ade3b0eb020a03b2a5fed4e6440
HorzaGobuchul <github@retrapur.fr>
2015-07-08 00:22:56 +0200

    Uploaded module template with all optional methods implemented.

---
+++ featureTemplate.js
@@ -0,0 +1,77 @@
+﻿AVE.Modules['ID'] = {
+    ID: 'ID',
+    Name: 'Name',
+    Desc: 'Desc',
+    Category: 'Cat',
+
+    Index: 100,
+    Debug: true,
+    Enabled: false,
+
+    Store: {},
+
+    Options: {
+        Enabled: {
+            Type: 'boolean',
+            Value: true,
+        },
+    },
+
+    OriginalOptions: "", //If ResetPref is used
+
+    SavePref: function (POST) {
+        var _this = AVE.Modules['ID']; //Change id here
+        POST = POST[_this.ID];
+
+        _this.Store.SetValue(_this.Store.Prefix + _this.ID, JSON.stringify(POST));
+    },
+
+    ResetPref: function () {// will add the reset option in the pref manager. Can be deleted.
+        var _this = AVE.Modules['ID']; //Change id here
+        _this.Options = JSON.parse(_this.OriginalOptions);
+    },
+
+    SetOptionsFromPref: function () {
+        var _this = AVE.Modules['ID'];
+        var Opt = _this.Store.GetValue(_this.Store.Prefix + _this.ID, "{}");
+
+        $.each(JSON.parse(Opt), function (key, value) {
+            _this.Options[key].Value = value;
+        });
+        _this.Enabled = _this.Options.Enabled.Value;
+    },
+
+    Load: function () {
+        this.Store = AVE.Storage;
+        this.OriginalOptions = JSON.stringify(this.Options); //If ResetPref is used
+        this.SetOptionsFromPref();
+
+        if (this.Enabled) {
+            this.Start();
+        }
+    },
+
+    Start: function () {
+        //this.AppendToPage();
+        //this.Listeners();
+    },
+
+    Update: function () {//Use if this module needs to be update by UpdateAfterLoadingMore, remove otherwise
+        this.Start();
+    },
+
+    AppendToPage: function () { //To insert content into the page
+    },
+
+    Listeners: function () { //To bind event listeneres to the content added in AppendToPage.
+    },
+
+    AppendToPreferenceManager: { //Use to add custom input to the pref Manager
+        html: function () {
+            var htmlStr = "";
+            return htmlStr;
+        },
+        callback: function () {
+        },
+    },
+};
